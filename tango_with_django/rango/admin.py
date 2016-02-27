0d3c8eff2b67d6b30ce62a9568073dbb15680a8c
Blagovest Tchasovnikarov <Blagovest Tchasovnikarov>
2016-02-27 14:00:32 +0000

    Login + Register

--- tango_with_django/rango/admin.py
+++ tango_with_django/rango/admin.py
@@ -1,13 +1,9 @@
 from django.contrib import admin
-from rango.models import Category, Page, UserProfile
+from rango.models import UserProfile
 
 
 # Add in this class to customized the Admin Interface
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
+
 
 # Update the registeration to include this customised interface
-admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
-
 admin.site.register(UserProfile)
--- tango_with_django/rango/views.py
+++ tango_with_django/rango/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from rango.models import Category,Page
-from rango.forms import CategoryForm,PageForm, UserForm, UserProfileForm
+from rango.forms import UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
@@ -12,12 +11,8 @@ def index(request):
     # Order the categories by no. likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
-    category_list = Category.objects.order_by('likes')[:5]
-    pages = Page.objects.order_by("views")[:5]
-    context_dict = {'categories': category_list,'pages':pages}
-
     # Render the response and send it back!
-    return render(request, 'rango/index.html',context_dict)
+    return render(request, 'rango/index.html')
 
 
 # Use the login_required() decorator to ensure only those logged in can access the view.
@@ -132,95 +127,3 @@ def user_login(request):
         # No context variables to pass to the template system, hence the
         # blank dictionary object...
         return render(request, 'rango/login.html', {})
-
-
-
-
-def add_page(request, category_name_slug):
-
-    try:
-        cat = Category.objects.get(slug=category_name_slug)
-    except Category.DoesNotExist:
-                cat = None
-
-    if request.method == 'POST':
-        form = PageForm(request.POST)
-        if form.is_valid():
-            if cat:
-                page = form.save(commit=False)
-                page.category = cat
-                page.views = 0
-                page.save()
-                # probably better to use a redirect here.
-                return category(request, category_name_slug)
-        else:
-            print form.errors
-    else:
-        form = PageForm()
-
-    context_dict = {'form':form, 'category': cat}
-
-    return render(request, 'rango/add_page.html', context_dict)
-
-
-def add_category(request):
-    # A HTTP POST?
-    if request.method == 'POST':
-        form = CategoryForm(request.POST)
-
-        # Have we been provided with a valid form?
-        if form.is_valid():
-            # Save the new category to the database.
-            form.save(commit=True)
-
-            # Now call the index() view.
-            # The user will be shown the homepage.
-            return index(request)
-        else:
-            # The supplied form contained errors - just print them to the terminal.
-            print form.errors
-    else:
-        # If the request was not a POST, display the form to enter details.
-        form = CategoryForm()
-
-    # Bad form (or form details), no form supplied...
-    # Render the form with error messages (if any).
-    return render(request, 'rango/add_category.html', {'form': form})
-
-
-
-
-
-def category(request, category_name_slug):
-
-    # Create a context dictionary which we can pass to the template rendering engine.
-    context_dict = {}
-
-    try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # So the .get() method returns one model instance or raises an exception.
-        category = Category.objects.get(slug=category_name_slug)
-        context_dict['category_name'] = category.name
-
-        # Retrieve all of the associated pages.
-        # Note that filter returns >= 1 model instance.
-        pages = Page.objects.filter(category=category)
-
-        # Adds our results list to the template context under name pages.
-        context_dict['pages'] = pages
-        # We also add the category object from the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
-        context_dict['category'] = category
-        context_dict['category_slug'] = category.slug
-    except Category.DoesNotExist:
-        # We get here if we didn't find the specified category.
-        # Don't do anything - the template displays the "no category" message for us.
-        pass
-
-    # Go render the response and return it to the client.
-    return render(request, 'rango/category.html', context_dict)	
-	
-	
-def about(request):
-    return render(request,"rango/about.html")
