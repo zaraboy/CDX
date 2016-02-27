9966f13d4f3570de272aab3d975e3acff919331f
Boris Lazarov <Boris Lazarov>
2016-02-27 13:32:47 +0000

    Still no idea

---
+++ tango_with_django/rango/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from rango.models import Category, Page, UserProfile
+
+
+# Add in this class to customized the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Update the registeration to include this customised interface
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
+
+admin.site.register(UserProfile)
